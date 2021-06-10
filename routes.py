da5a00138fb23c12c06942dd9f2a2157f3ee2416
pcborges <delyto@gmail.com>
2021-06-10 17:24:58 -0300

    Inicializando o projeto no GIT

---
+++ routes.py
@@ -0,0 +1,55 @@
+import os
+import time
+from flask import Flask, flash, request, redirect, url_for, abort, render_template
+from werkzeug.utils import secure_filename
+from src.tratar_dados import getProjetosEmExecucaoHTML, getProjetosPriorizadosJSON, getProjetosEmDiagnosticoHTML
+#"D:\\Patrick\\Ministerio Economia\\Projeto Situacao Startups\\upload"
+UPLOAD_FOLDER = os.getcwd() + 'upload'
+ALLOWED_EXTENSIONS = {'xlsm', 'xlsx'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+app = Flask("Projeto")
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/dashboard', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            abort(400, "file no in request.files")
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            abort(400, "filename em branco")
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join("upload", filename))
+        else:
+            abort(400, "arquivo nao permitido")
+
+    path = os.getcwd() + '\\upload\\' + filename
+    #"D:\\Patrick\\Ministerio Economia\\Projeto Situacao Startups\\upload\\" + filename
+    projetosJson = getProjetosPriorizadosJSON(path)
+    print(type(projetosJson[0]['status']))
+    html_emExecucao = getProjetosEmExecucaoHTML(path)
+    html_emDiagnostico = getProjetosEmDiagnosticoHTML(path)
+    dataModificacao = time.strftime(
+        '%d/%m/%Y', time.localtime(os.path.getmtime(path)))
+    # print(dataDF.head())
+    return render_template('dashboard.html', projetos=projetosJson, execucao=html_emExecucao, diagnostico=html_emDiagnostico, dataModificacao=dataModificacao)
+
+
+app.run(debug=True)
---
+++ src/tratar_dados.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import json
+
+
+def getIndicadoresProjetos(path, index_mes_ano_indicador=16):
+    df = pd.read_excel(path, sheet_name='KPIs', header=1)
+    #startupsDF = aba1.loc[0:, ['Nome do projeto','Sigla Orgão', 'Status do Projeto', 'Líder do SQUAD']]
+    # referenciaEsperado 16 é o mês de Maio/21 (número da coluna da planilha transformada no dataframe)
+    referenciaEsperado = index_mes_ano_indicador
+    referenciaRealizado = referenciaEsperado + 25
+    kpisDF = df.iloc[0:, [1, 3, 6, referenciaEsperado, referenciaRealizado]]
+    kpisDF.columns.values[3] = 'Esperado'
+    kpisDF.columns.values[4] = 'Realizado'
+    # kpisDF.columns
+    #kpisDF.loc[kpisDF['Esperado Mai/21'] > kpisDF['Realizado Mai/21'], 'KPI Maio/2021'] = 'Atrasado'
+    kpisDF.fillna(value={'Esperado': 0, 'Realizado': 0,
+                  'KPI': ''}, inplace=True)
+    kpisDF['Farol'] = kpisDF.apply(getIndicadorKPI, axis=1)
+    kpisDF = kpisDF.reindex(
+        columns=['Nome do projeto', 'KPI', 'Esperado', 'Realizado', 'Farol'])
+    kpisDF['Nome do projeto'] = kpisDF['Nome do projeto'].str.lower()
+    execucaoDF = kpisDF[kpisDF['KPI'].str.contains("Exec")]
+    execucaoDF = execucaoDF.reset_index(drop=True)
+    entregasDF = kpisDF[kpisDF['KPI'].str.contains("Entregas")]
+    entregasDF = entregasDF.reset_index(drop=True)
+    dfFinal = getDFComIndicadorConsolidado(execucaoDF, entregasDF)
+    farol = {4: 'VERDE', 3: 'AMARELO', 2: 'VERMELHO', 1: 'CINZA'}
+    dfFinal['Farol'].replace(farol, inplace=True)
+    dfFinal.drop(['KPI', 'Esperado', 'Realizado'],
+                 axis='columns', inplace=True)
+    # return dfFinal.to_json(orient="split")
+    return dfFinal
+
+
+def getProjetosEmExecucaoHTML(path, index_mes_ano_indicador=16):
+    df = pd.read_excel(path, sheet_name='Startups')
+    df = df.loc[0:, ['Nome do projeto', 'Sigla Orgão',
+                     'Status do Projeto', 'Líder do SQUAD']]
+    df.fillna(value={'Líder do SQUAD': 'N/D'}, inplace=True)
+    df['Nome do projeto'] = df['Nome do projeto'].str.lower()
+    emExecucaoDF = df[df['Status do Projeto'].str.contains("Exec")]
+    indicadoresDF = getIndicadoresProjetos(path, index_mes_ano_indicador)
+    projetosEmExecucaoDFTotal = pd.merge(
+        emExecucaoDF, indicadoresDF, how='left', on='Nome do projeto')
+    projetosEmExecucaoDFTotal.fillna(value={'Farol': 'CINZA'})
+    return projetosEmExecucaoDFTotal.to_html(index=False, classes=['table', 'table-striped'], justify='left', table_id='emExecucao')
+
+
+def getProjetosEmDiagnosticoHTML(path, index_mes_ano_indicador=16):
+    df = pd.read_excel(path, sheet_name='Startups')
+    df = df.loc[0:, ['Nome do projeto', 'Sigla Orgão',
+                     'Status do Projeto', 'Líder do SQUAD']]
+    df['Nome do projeto'] = df['Nome do projeto'].str.lower()
+    emExecucaoDF = df[df['Status do Projeto'].str.contains(
+        "Diagn") | df['Status do Projeto'].str.contains("Prioriza")]
+    indicadoresDF = getIndicadoresProjetos(path, index_mes_ano_indicador)
+    projetosEmDiagnosticoTotal = pd.merge(
+        emExecucaoDF, indicadoresDF, how='left', on='Nome do projeto')
+    projetosEmDiagnosticoTotal.fillna(value={'Farol': 'CINZA'})
+    return projetosEmDiagnosticoTotal.to_html(index=False, classes=['table', 'table-striped'], justify='left')
+
+
+def getProjetosPriorizadosJSON(path):
+    df = pd.read_excel(path, sheet_name='Startups')
+    df = df[['Nome do projeto', 'Status do Projeto',
+             'Líder do SQUAD', 'Sigla Orgão']]
+    indexRemove = df[df['Status do Projeto'] == '1-Em espera'].index
+    df.drop(indexRemove, inplace=True)
+    df.fillna(value={'Líder do SQUAD': 'N/D'}, inplace=True)
+    df.rename(columns={'Nome do projeto': 'nome', 'Status do Projeto': 'status',
+              'Líder do SQUAD': 'lider', 'Sigla Orgão': 'sigla_orgao'}, inplace=True)
+    status = {'5-Execução': 'execucao', '3-Diagnóstico': 'diagnostico',
+              '2-Priorização Estratégica': 'priorizacao'}
+    df['status'].replace(status, inplace=True)
+    dfJson = df.to_json(orient='records')
+    return json.loads(dfJson)
+    #df.to_html(index=False, classes=['table', 'table-striped'],  justify='left')
+
+
+def getIndicadorKPI(linha):
+    try:
+        # if linha['Esperado Mai/21'] == None | linha['Realizado Mai/21'] == None:
+        # return 'CINZA'
+        if ((linha['Realizado'] * 100) / linha['Esperado']) >= 85:
+            return 4  # VERDE
+        elif (((linha['Realizado'] * 100) / linha['Esperado']) >= 70) & (((linha['Realizado'] * 100) / linha['Esperado']) < 85):
+            return 3  # AMARELO
+        else:
+            return 2  # VERMELHO
+    except ZeroDivisionError:
+        return 1  # CINZA
+
+
+def getDFComIndicadorConsolidado(execucao, entregas):
+    data = []
+    for index, row in execucao.iterrows():
+        row2 = entregas.iloc[index, -1]
+        if row['Farol'] == row2:
+            data.append(row)
+        elif row['Farol'] > row2:
+            data.append(entregas.iloc[index])
+        else:
+            data.append(row)
+
+    return pd.DataFrame(data)
