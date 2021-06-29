c0d53f525171f4fa399d459fcdec8f4c38b449fa
pcborges <delyto@gmail.com>
2021-06-29 13:02:34 -0300

    remover getCredentials

--- src/utils.py
+++ src/utils.py
@@ -6,27 +6,6 @@ UPLOAD_FOLDER = os.getcwd() + 'upload'
 ALLOWED_EXTENSIONS = {'xlsm', 'xlsx'}
 
 
-def getGoogleCredentials():
-    PROJECT_ID = os.environ.get("PROJECT_ID")
-    PRIVATE_KEY_ID = os.environ.get("PRIVATE_KEY_ID")
-    PRIVATE_KEY = os.environ.get("PRIVATE_KEY")
-    CLIENT_EMAIL = os.environ.get("CLIENT_EMAIL")
-    CLIENT_ID = os.environ.get("CLIENT_ID")
-
-    print("KEYS", PROJECT_ID, CLIENT_ID)
-
-    with open(os.getcwd() + '/credentials/sgdkeybigquery.json', 'r') as f:
-        credenciais = json.load(f)
-
-    credenciais["project_id"] = PROJECT_ID
-    credenciais["private_key_id"] = PRIVATE_KEY_ID
-    credenciais["private_key"] = PRIVATE_KEY
-    credenciais["client_email"] = CLIENT_EMAIL
-    credenciais["client_id"] = CLIENT_ID
-
-    return credenciais
-
-
 def validateFileReq(request):
     # check if the post request has the file part
     if 'file' not in request.files:
