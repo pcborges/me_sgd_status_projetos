b88b66627ee1197cbb9f6c1ac5447e5185d53413
Patrick Borges <delyto@gmail.com>
2021-07-06 17:28:29 -0300

    adiconar upload novo arquivo

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,4 +1,6 @@
-window.addEventListener("submit", () => {
+window.addEventListener("submit", (e) => {
+  e.stopPropagation()
+  console.log(this)
   const loader = document.getElementById("loader");
   loader.classList.remove("hidden"); // class "loader hidden"
 });
